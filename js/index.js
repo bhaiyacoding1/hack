d04501f2c2538349391684f2db338998287296ea
Web_Devlopment_With_BCA <120241475+bhaiyacoding1@users.noreply.github.com>
2024-03-11 18:33:56 +0530

    add compiler file

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-document.querySelector("#open").addEventListener("click",()=>{
-    // console.log("jyy");
-    document.querySelector(".mobile-nav").style.right = "0";
-    document.querySelector(".hero-section").style.zIndex = "4";
+document.querySelector("#open").addEventListener("click", () => {
+  // console.log("jyy");
+  document.querySelector(".mobile-nav").style.right = "0";
+  document.querySelector(".hero-section").style.zIndex = "4";
 })
 document.querySelector("#close").addEventListener("click", () => {
-    // console.log("by");
-    document.querySelector(".mobile-nav").style.right = "-100%";
-    document.querySelector(".hero-section").style.zIndex = "5";
+  // console.log("by");
+  document.querySelector(".mobile-nav").style.right = "-100%";
+  document.querySelector(".hero-section").style.zIndex = "5";
 
 })
 
@@ -59,3 +59,44 @@ const initOverlayCard = (cardEl) => {
 
 cards.forEach(initOverlayCard);
 document.body.addEventListener("pointermove", applyOverlayMask);
+
+
+// ---------------------------typewriter---------------------------
+
+document.addEventListener('DOMContentLoaded', function () {
+  const text = `
+import React from 'react';
+  function App() {
+    return (
+      <div>
+        <h1>Hello,Friend</h1>
+        <p>Wlcome to Code Bounty</p>
+      </div>
+       );
+    }
+  export default App;
+`;
+
+  const codeElement = document.getElementById("react-code");
+  const delay = 40; // Delay in milliseconds between each character
+  let index = 0;
+
+  function typeWriter() {
+    if (index < text.length) {
+      codeElement.textContent += text.charAt(index);
+      index++;
+      setTimeout(typeWriter, delay);
+    }
+  }
+
+  // Start typing when the DOM content is loaded
+  typeWriter();
+});
+
+// -------------------------vanilla-js-card-hover---------------------
+
+VanillaTilt.init(document.querySelectorAll(".type-rider-div,.card"), {
+  max: 8,
+  speed: 200
+});
+
