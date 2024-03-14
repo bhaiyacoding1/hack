f7bb3e87859c48283d8ffac35d6edaeb4aa27082
Web_Devlopment_With_BCA <120241475+bhaiyacoding1@users.noreply.github.com>
2024-03-14 15:34:24 +0530

    add internship section

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-document.querySelector("#open").addEventListener("click", () => {
-  // console.log("jyy");
-  document.querySelector(".mobile-nav").style.right = "0";
-  document.querySelector(".hero-section").style.zIndex = "4";
-})
-document.querySelector("#close").addEventListener("click", () => {
-  // console.log("by");
-  document.querySelector(".mobile-nav").style.right = "-100%";
-  document.querySelector(".hero-section").style.zIndex = "5";
+// document.querySelector("#open").addEventListener("click", () => {
+//   console.log("jyy");
+//   document.querySelector(".mobile-nav").style.right = "0";
+//   document.querySelector(".hero-section").style.zIndex = "4";
+// })
+// document.querySelector("#close").addEventListener("click", () => {
+//   console.log("by");
+//   document.querySelector(".mobile-nav").style.right = "-100%";
+//   document.querySelector(".hero-section").style.zIndex = "5";
 
-})
+// })
 
 
 // -----------------CARD JAVASCRIPT---------------------
